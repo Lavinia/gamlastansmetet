0e182a0f8ce712b0e6772de35af9ba535fb1c40b
Lennart and Kim <lennart@devl.se>
2015-10-30 15:22:10 +0100

    Handle signups without required params.

--- test/controllers/signup_controller_test.exs
+++ test/controllers/signup_controller_test.exs
@@ -15,4 +15,12 @@ defmodule Gamlastansmetet.SignupControllerTest do
     assert conn.resp_body =~ "Tack för att du anmält Kämpetorskarna"
     assert Signup.count == signup_count + 1
   end
+
+  test "POST /signup without required params" do
+    signup_count = Signup.count
+    conn = post conn(), "/signup", %{ "signup" => %{ "phone" => "123-456789", "email" => "kämpe@torskar.na" } }
+    assert conn.status == Plug.Conn.Status.code :ok
+    assert conn.resp_body =~ "Anmälan 2015"
+    assert Signup.count == signup_count
+  end
 end
--- web/controllers/signup_controller.ex
+++ web/controllers/signup_controller.ex
@@ -9,11 +9,17 @@ defmodule Gamlastansmetet.SignupController do
   end
 
   def create(conn, params) do
-    {:ok, signup} = Signup.changeset(%Signup{}, params["signup"])
-    |> Repo.insert
+    changeset = Signup.changeset(%Signup{}, params["signup"])
 
-    conn
-    |> put_status(Status.code(:created))
-    |> render "show.html", signup: signup
+    {status, signup_or_changeset} = changeset |> Repo.insert
+    if status == :ok do
+      conn
+      |> put_status(Status.code(:created))
+      |> render "show.html", signup: signup_or_changeset
+    else
+      conn
+      |> put_status(Status.code(:ok))
+      |> render "new.html"
+    end
   end
 end
