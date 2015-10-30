f46045713d73d6b5eef8c1f11b0e6cf84ebeba24
Lennart and Kim <lennart@devl.se>
2015-10-30 15:04:17 +0100

    Refactor signup tests

--- test/models/signup_test.exs
+++ test/models/signup_test.exs
@@ -3,29 +3,31 @@ defmodule Gamlastansmetet.SignupTest do
   alias Gamlastansmetet.Signup
 
   test "changeset is not valid when team name is missing" do
-    changeset = Signup.changeset(%Signup{}, %{"email" => "kämpe@torskar.na"})
-    refute changeset.valid?
+    params = %{"email" => "kämpe@torskar.na"}
+    refute changeset_for(params).valid?
   end
 
   test "changeset is not valid when the team name is blank" do
-    changeset = Signup.changeset(%Signup{}, %{"team_name" => "", "phone" => "123-456789" })
-    refute changeset.valid?
+    params = %{"team_name" => "", "phone" => "123-456789"}
+    refute changeset_for(params).valid?
   end
 
   test "changeset is not valid without a phone or email" do
-    changeset = Signup.changeset(%Signup{}, %{"team_name" => "Kämpetorskarna"})
-    refute changeset.valid?
+    params = %{"team_name" => "Kämpetorskarna"}
+    refute changeset_for(params).valid?
   end
 
   test "changeset is valid with team name and phone" do
     params = %{"team_name" => "Kämpetorskarna", "phone" => "123-456789"}
-    changeset = Signup.changeset(%Signup{}, params)
-    assert changeset.valid?
+    assert changeset_for(params).valid?
   end
 
   test "changeset is valid with team name and email" do
     params = %{"team_name" => "Torsksimmarna", "email" => "torskar@simmar.se"}
-    changeset = Signup.changeset(%Signup{}, params)
-    assert changeset.valid?
+    assert changeset_for(params).valid?
+  end
+
+  def changeset_for(params) do
+    Signup.changeset(%Signup{}, params)
   end
 end
